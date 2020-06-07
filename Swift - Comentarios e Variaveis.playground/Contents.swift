4e6c11570748b79d31a396e208363c30367db2b0
Bruno Alves <alves.bruno945@gmail.com>
2020-06-07 01:58:54 -0300

    VariablesSwift
    A simple Swift variable file

---
+++ Swift - Comentarios e Variaveis.playground/Contents.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+// Variaveis - Algo que muda durante o codigo - Abaixo exemplo de variavel
+var age: Int = 21
+
+// Constante - Para representar uma constante e utilizado a palavra LET
+let name: String = "Bruno"
+
+// \(variavel) - representa uma interpolacao
+print("Meu nome e \(name) e eu tenho \(age) anos")
